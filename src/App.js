7685623fc675240e804a0cabb01a6eadef150664
roscaandreea <andreea.rosca@info.uaic.ro>
2021-03-22 14:27:50 +0200

    Fetch data to the api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Cards, Charts, CountryPicker } from './components';
+import { fetchData } from './api';
 import styles from './App.module.css';
 
 class App extends React.Component{
-  render(){
+    state ={ 
+        data: {},
+    }
+    async componentDidMount(){
+        const fetchedData = await fetchData();
+        this.setState({data: fetchedData});
+    }
+
+    render(){
+      const data = this.state.data;
       return(
           <div className={styles.container}>
-              <Cards />
+              <Cards data={data}/>
               <CountryPicker />
               <Charts />
           </div>
---
+++ src/api/index.js
@@ -0,0 +1,19 @@
+//help to make request to api
+import axios from 'axios';
+
+const url="https://covid19.mathdro.id/api";
+export const fetchData = async() => {
+    try {
+        const { data: {confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+        const modifiedData ={
+            confirmed: confirmed,
+            recovered: recovered,
+            deaths: deaths,
+            lastUpdate: lastUpdate
+        };
+        return modifiedData;
+
+    } catch(err) {
+
+    }
+}
