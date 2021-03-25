1a23a10d29ecb9c80edb4374654554305299f558
roscaandreea <andreea.rosca@info.uaic.ro>
2021-03-25 14:56:47 +0200

    centralized the data for each country

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,13 @@
 import axios from 'axios';
 
 const url="https://covid19.mathdro.id/api";
-export const fetchData = async() => {
+export const fetchData = async(country) => {
+    let changeableURL = url;
+    if(country){
+        changeableURL = `${url}/countries/${country}`;
+    }
     try {
-        const { data: {confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+        const { data: {confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableURL);
         const modifiedData ={
             confirmed: confirmed,
             recovered: recovered,
@@ -14,7 +18,7 @@ export const fetchData = async() => {
         return modifiedData;
 
     } catch(err) {
-
+        console.log(err);
     }
 }
 
@@ -28,6 +32,15 @@ export const fetchDailyData = async() => {
         }));
         return modifiedData;
     }catch (err){
-        console.log('this is an error there');
+        console.log(err);
+    }
+}
+
+export const fetchCountries = async() => {
+    try{
+        const {data: { countries }} = await axios.get(`${url}/countries`);
+        return countries.map((country) => country.name);
+    } catch(err) {
+        console.log(err);
     }
 }
