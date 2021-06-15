7eee993a12bed95f154cdcc44371b9f5adb4f4f4
roscaandreea <andreea.rosca@info.uaic.ro>
2021-06-15 17:57:32 +0300

    Added fisrt code for map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import Loading from './Loading'
+import Loading from './Loading';
+import CovidMap from './CovidMap';
+import Legend from './Legend';
+import LoadCountries from '../../tasks/LoadCountries';
 
 const Map = () => {
-    const [ countrie, setCountries ]= useState([]);
+    const [ countries, setCountries ]= useState([]);
+    const load = () =>{
+        const loadCountries= new LoadCountries();
+        loadCountries.load(setCountries);
+    };
+    useEffect(load,[]);
     return(
         <div>
+            { countries.length === 0 ? (
             <Loading />
+             ) : ( 
+             <div>
+                 <CovidMap />
+                 <Legend />
+             </div> 
+            )}
         </div>
     );
 }
---
+++ src/tasks/LoadCountries.js
@@ -0,0 +1,9 @@
+import countries from '../data/countries.json';
+
+class LoadCountries{
+    load =(setState) =>{
+        setState()
+    }
+}
+
+export default LoadCountries;
